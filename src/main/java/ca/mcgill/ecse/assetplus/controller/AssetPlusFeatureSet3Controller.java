9cf087e0b6f48956f0fb91713c6d436f38e33181
Houman <veryhouman@gmail.com>
2023-10-17 19:04:14 -0400

    -Specific Asset controller completed

--- src/main/java/ca/mcgill/ecse/assetplus/controller/AssetPlusFeatureSet3Controller.java
+++ src/main/java/ca/mcgill/ecse/assetplus/controller/AssetPlusFeatureSet3Controller.java
@@ -6,19 +6,70 @@ public class AssetPlusFeatureSet3Controller {
 
   public static String addSpecificAsset(int assetNumber, int floorNumber, int roomNumber,
       Date purchaseDate, String assetTypeName) {
-    // Remove this exception when you implement this method
-    throw new UnsupportedOperationException("Not Implemented!");
+        //Checks if assetNumber is greater or equal to one
+        if (assetNumber < 1) {
+          return "Asset number must be greater than or equal to one.";
+        }
+
+        //Checks if floorNumber is greater or equal to one
+        if (floorNumber < 0) {
+          return "Floor number must be greater or equal to zero.";
+        }
+
+        //Checks if roomNumber is greater than or equal to -1
+        if (roomNumber < -1) {
+          return "Room number must be greater than or equal to minus one.";
+        }
+
+        //Add Asset
+        SpecificAsset mySpecificAsset = new SpecificAsset(assetNumber, floorNumber, roomNumber, purchaseDate, assetTypeName);
+        return "Specific asset added successfully!";
   }
 
   public static String updateSpecificAsset(int assetNumber, int newFloorNumber, int newRoomNumber,
       Date newPurchaseDate, String newAssetTypeName) {
-    // Remove this exception when you implement this method
-    throw new UnsupportedOperationException("Not Implemented!");
+        //Checks if assetNumber is greater or equal to one
+        if (assetNumber < 1) {
+          return "Asset number must be greater than or equal to one.";
+        }
+
+        //Checks if floorNumber is greater or equal to one
+        if (newFloorNumber < 0) {
+          return "New floor number must be greater or equal to zero.";
+        }
+
+        //Checks if roomNumber is greater than or equal to -1
+        if (newRoomNumber < -1) {
+          return "New room number must be greater than or equal to minus one.";
+        }
+
+        //Checks if the asset exists in the first place
+        SpecificAsset mySpecificAsset = SpecificAsset.getSpecificAsset(assetNumber);
+        if (mySpecificAsset == null) {
+          return "Specific asset with this asset number does not exist.";
+        }
+
+        //Update the specific asset
+        mySpecificAsset.setFloorNumber(newFloorNumber);
+        mySpecificAsset.setRoomNumber(newRoomNumber);
+        mySpecificAsset.setPurchaseDate(newPurchaseDate); 
+        mySpecificAsset.setAssetType(newAssetTypeName);
+        return "Specific asset updated successfully";
   }
 
   public static void deleteSpecificAsset(int assetNumber) {
-    // Remove this exception when you implement this method
-    throw new UnsupportedOperationException("Not Implemented!");
+    //Check if already at minimum number of specific assets
+    if (!hasSpecificAssets()) {
+      return "Already at minimum number of specific assets";
+    }
+
+    //Check if the asset with this number exists
+    SpecificAsset mySpecificAsset = SpecificAsset.getSpecificAsset(assetNumber);
+        if (mySpecificAsset == null) {
+          return "Specific asset with this asset number does not exist.";
+        }
+
+   mySpecificAsset.delete();
   }
 
 }
