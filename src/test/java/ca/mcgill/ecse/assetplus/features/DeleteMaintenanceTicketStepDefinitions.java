db18b43fd8677e1dac1a302c22a98e0603eb4a8b
Mathieu <matpestel@gmail.com>
2023-10-19 14:11:50 -0400

    Update DeleteMaintenanceTicketStepDefinitions.java
    Formatted file with Google java format and used Date.valueOf instead

--- src/test/java/ca/mcgill/ecse/assetplus/features/DeleteMaintenanceTicketStepDefinitions.java
+++ src/test/java/ca/mcgill/ecse/assetplus/features/DeleteMaintenanceTicketStepDefinitions.java
@@ -1,14 +1,10 @@
 package ca.mcgill.ecse.assetplus.features;
 
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
 import java.util.List;
-import java.util.Locale;
 import java.util.Map;
 import java.sql.Date;
 
 import ca.mcgill.ecse.assetplus.application.AssetPlusApplication;
-import ca.mcgill.ecse.assetplus.controller.AssetPlusFeatureSet3Controller;
 import ca.mcgill.ecse.assetplus.controller.AssetPlusFeatureSet4Controller;
 import ca.mcgill.ecse.assetplus.model.*;
 import io.cucumber.java.en.Given;
@@ -23,17 +19,18 @@ public class DeleteMaintenanceTicketStepDefinitions {
 
   /**
    * Adding employees given in the first @Given clause in Gherkin feature file.
+   *
    * @author Isbat-ul Islam
    * @param dataTable
    */
   @Given("the following employees exist in the system \\(p7)")
   public void the_following_employees_exist_in_the_system_p7(
           io.cucumber.datatable.DataTable dataTable) {
-    List<Map<String,String>> rows = dataTable.asMaps();
-    for (var row: rows) {
-      String email       = row.get("email");
-      String password    = row.get("password");
-      String name        = row.get("name");
+    List<Map<String, String>> rows = dataTable.asMaps();
+    for (var row : rows) {
+      String email = row.get("email");
+      String password = row.get("password");
+      String name = row.get("name");
       String phoneNumber = row.get("phoneNumber");
       assetPlus.addEmployee(email, name, password, phoneNumber);
     }
@@ -41,6 +38,7 @@ public class DeleteMaintenanceTicketStepDefinitions {
 
   /**
    * Adding Manager from the feature file.
+   *
    * @author Isbat-ul Islam
    * @param dataTable
    */
@@ -48,8 +46,8 @@ public class DeleteMaintenanceTicketStepDefinitions {
   public void the_following_manager_exists_in_the_system_p7(
           io.cucumber.datatable.DataTable dataTable) {
 
-    List<Map<String,String>> rows = dataTable.asMaps();
-    for (var row: rows) {
+    List<Map<String, String>> rows = dataTable.asMaps();
+    for (var row : rows) {
       String aEmail = row.get("email");
       String aPassword = row.get("password");
       String aName = "ManagerName";
@@ -61,6 +59,7 @@ public class DeleteMaintenanceTicketStepDefinitions {
 
   /**
    * Adds asset types specified in the Gherkin Feature file.
+   *
    * @author Isbat-ul Islam
    * @param dataTable
    */
@@ -68,8 +67,8 @@ public class DeleteMaintenanceTicketStepDefinitions {
   public void the_following_asset_types_exist_in_the_system_p7(
           io.cucumber.datatable.DataTable dataTable) {
 
-    List<Map<String,String>> rows = dataTable.asMaps();
-    for(var row: rows) {
+    List<Map<String, String>> rows = dataTable.asMaps();
+    for (var row : rows) {
       String aName = row.get("name");
       Integer aExpectedLifeSpan = Integer.parseInt(row.get("expectedLifeSpan"));
       assetPlus.addAssetType(aName, aExpectedLifeSpan);
@@ -78,6 +77,7 @@ public class DeleteMaintenanceTicketStepDefinitions {
 
   /**
    * Adds specific assets defined in Gherkin feature file.
+   *
    * @author Isbat-ul Islam
    * @param dataTable
    */
@@ -85,49 +85,53 @@ public class DeleteMaintenanceTicketStepDefinitions {
   public void the_following_assets_exist_in_the_system_p7(
           io.cucumber.datatable.DataTable dataTable) {
 
-    List<Map<String,String>> rows = dataTable.asMaps(String.class, String.class);
-    for(var row: rows){
+    List<Map<String, String>> rows = dataTable.asMaps(String.class, String.class);
+    for (var row : rows) {
       Integer aAssetNumber = Integer.parseInt(row.get("assetNumber"));
       AssetType aAssetType = AssetType.getWithName(row.get("type"));
-      Date aDate           = Date.valueOf(row.get("purchaseDate"));
+      Date aDate = Date.valueOf(row.get("purchaseDate"));
       Integer aFloorNumber = Integer.parseInt(row.get("floorNumber"));
-      Integer aRoomNumber  = Integer.parseInt(row.get("roomNumber"));
+      Integer aRoomNumber = Integer.parseInt(row.get("roomNumber"));
       assetPlus.addSpecificAsset(aAssetNumber, aFloorNumber, aRoomNumber, aDate, aAssetType);
     }
   }
 
   /**
    * Add specified Tickets defined in feature file.
+   *
    * @author Isbat-ul Islam
    * @param dataTable
    */
   @Given("the following tickets exist in the system \\(p7)")
   public void the_following_tickets_exist_in_the_system_p7(
           io.cucumber.datatable.DataTable dataTable) {
-    List<Map<String,String>> rows = dataTable.asMaps();
-    for (var row: rows){
+    List<Map<String, String>> rows = dataTable.asMaps();
+    for (var row : rows) {
       Integer aID = Integer.parseInt(row.get("id"));
       String aTicketRaiserEmail = row.get("ticketRaiser");
       Date dateRaised = Date.valueOf(row.get("raisedOnDate"));
       String description = row.get("description");
       Integer assetID = Integer.parseInt(row.get("assetNumber"));
-      AssetPlusFeatureSet4Controller.addMaintenanceTicket(aID, dateRaised, description, aTicketRaiserEmail, assetID);
+      AssetPlusFeatureSet4Controller.addMaintenanceTicket(
+              aID, dateRaised, description, aTicketRaiserEmail, assetID);
     }
   }
+
   /**
    * Check if the number of maintenance tickets in the system is correct
+   *
    * @author Neil Joe George
    * @param string
    */
   @When("the manager attempts to delete the maintenance ticket with id {string} \\(p7)")
   public void the_manager_attempts_to_delete_the_maintenance_ticket_with_id_p7(String string) {
-    // Write code here that turns the phrase above into concrete actions
     int aID = Integer.parseInt(string);
     AssetPlusFeatureSet4Controller.deleteMaintenanceTicket(aID);
   }
 
   /**
    * Check if the maintenance tickets saved in the system are correct
+   *
    * @author Mathieu Pestel
    * @author Neil Joe George
    * @param dataTable
@@ -136,15 +140,10 @@ public class DeleteMaintenanceTicketStepDefinitions {
   public void the_following_tickets_shall_exist_in_the_system_p7(
           io.cucumber.datatable.DataTable dataTable) {
     List<Map<String, String>> ticketTable = dataTable.asMaps(String.class, String.class);
-    for (Map<String, String> ticket: ticketTable){
+    for (Map<String, String> ticket : ticketTable) {
       int ticketID = Integer.parseInt(ticket.get("id"));
       MaintenanceTicket currentTicket = MaintenanceTicket.getWithId(ticketID);
-      java.util.Date currentDate = null;
-      try {
-        currentDate = (java.util.Date) new SimpleDateFormat("yyyy-MM-dd").parse(ticket.get("raisedOnDate"));
-      } catch (ParseException e) {
-        System.out.println("Issue");
-      }
+      Date currentDate = Date.valueOf(ticket.get("raisedOnDate"));
       String currentDescription = currentTicket.getDescription();
       int currentAssetNumber = currentTicket.getAsset().getAssetNumber();
 
@@ -153,11 +152,11 @@ public class DeleteMaintenanceTicketStepDefinitions {
       assertEquals(ticket.get("description"), currentDescription);
       assertEquals(Integer.parseInt(ticket.get("assetNumber")), currentAssetNumber);
     }
-
   }
 
   /**
    * Check if the number of maintenance tickets in the system is correct
+   *
    * @author Mathieu Pestel
    * @param string
    */
