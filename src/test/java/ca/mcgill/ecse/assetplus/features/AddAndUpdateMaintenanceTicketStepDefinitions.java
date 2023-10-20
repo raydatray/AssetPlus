2c0da9ce36b23c3da008e3065f98fc7797033edc
Nicolas-Bolouri <nicolasbolouri@gmail.com>
2023-10-19 21:17:39 -0400

    fixed PR comments

--- src/test/java/ca/mcgill/ecse/assetplus/features/AddAndUpdateMaintenanceTicketStepDefinitions.java
+++ src/test/java/ca/mcgill/ecse/assetplus/features/AddAndUpdateMaintenanceTicketStepDefinitions.java
@@ -1,6 +1,7 @@
 package ca.mcgill.ecse.assetplus.features;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.sql.Date;
@@ -18,7 +19,6 @@ public class AddAndUpdateMaintenanceTicketStepDefinitions {
 
   private static AssetPlus assetPlus = AssetPlusApplication.getAssetPlus();
   private String error;
-  private int errorCntr;
 
   /**
    * @author Tiffany Miller
@@ -37,7 +37,6 @@ public class AddAndUpdateMaintenanceTicketStepDefinitions {
       assetPlus.addEmployee(email, name, password, phoneNumber);
     }
     error = "";
-    errorCntr = 0;
 
   }
 
@@ -57,7 +56,6 @@ public class AddAndUpdateMaintenanceTicketStepDefinitions {
       assetPlus.setManager(manager);
     }
     error = "";
-    errorCntr = 0;
 
   }
 
@@ -78,7 +76,6 @@ public class AddAndUpdateMaintenanceTicketStepDefinitions {
       assetPlus.addGuest(email, name, password, phoneNumber);
     }
     error = "";
-    errorCntr = 0;
 
   }
 
@@ -97,7 +94,6 @@ public class AddAndUpdateMaintenanceTicketStepDefinitions {
       assetPlus.addAssetType(name, lifeSpan);
     }
     error = "";
-    errorCntr = 0;
 
   }
 
@@ -119,7 +115,6 @@ public class AddAndUpdateMaintenanceTicketStepDefinitions {
       assetPlus.addSpecificAsset(assetNumber, floorNumber, roomNumber, purchaseDate, type);
     }
     error = "";
-    errorCntr = 0;
   }
 
   /**
@@ -144,7 +139,6 @@ public class AddAndUpdateMaintenanceTicketStepDefinitions {
       thisTicket.setAsset(asset);
     }
     error = "";
-    errorCntr = 0;
   }
 
   /**
@@ -157,7 +151,8 @@ public class AddAndUpdateMaintenanceTicketStepDefinitions {
    */
   @When("the user with email {string} attempts to add a new maintenance ticket to the system with id {string}, date {string}, description {string}, and asset number {string} \\(p16)")
   public void the_user_with_email_attempts_to_add_a_new_maintenance_ticket_to_the_system_with_id_date_description_and_asset_number_p16(
-      String email, String idString, String dateString, String description, String assetNumberString) {
+      String email, String idString, String dateString, String description,
+      String assetNumberString) {
 
     int id = Integer.parseInt(idString);
     Date date = Date.valueOf(dateString);
@@ -218,7 +213,8 @@ public class AddAndUpdateMaintenanceTicketStepDefinitions {
    */
   @When("the manager attempts to update the maintenance ticket with id {string} to ticket raiser {string}, date {string}, description {string}, and asset number {string} \\(p16)")
   public void the_manager_attempts_to_update_the_maintenance_ticket_with_id_to_ticket_raiser_date_description_and_asset_number_p16(
-      String idString, String email, String dateString, String description, String assetNumberString) {
+      String idString, String email, String dateString, String description,
+      String assetNumberString) {
 
     int id = Integer.parseInt(idString);
     Date date = Date.valueOf(dateString);
@@ -230,7 +226,8 @@ public class AddAndUpdateMaintenanceTicketStepDefinitions {
 
   /**
    * @author Tiffany Miller
-   * @param expectedNumTicketsString The number of maintenance tickets that is expected in the system.
+   * @param expectedNumTicketsString The number of maintenance tickets that is expected in the
+   *        system.
    */
   @Then("the number of tickets in the system shall be {string} \\(p16)")
   public void the_number_of_tickets_in_the_system_shall_be_p16(String expectedNumTicketsString) {
@@ -248,33 +245,21 @@ public class AddAndUpdateMaintenanceTicketStepDefinitions {
   public void the_following_tickets_shall_exist_in_the_system_p16(
       io.cucumber.datatable.DataTable dataTable) {
 
-    List<List<String>> expectedTickets = new ArrayList<List<String>>();
-    List<List<String>> actualTickets = new ArrayList<List<String>>();
+    Map<String, List<String>> expectedTicketsMap = new HashMap<>();
+    Map<String, List<String>> actualTicketsMap = new HashMap<>();
+
     List<List<String>> rows = dataTable.asLists(String.class);
+
     for (int i = 1; i < rows.size(); i++) {
       List<String> row = rows.get(i);
-      List<String> oneTicket = new ArrayList<String>();
       String id = row.get(0);
-      String ticketRaiser = row.get(1);
-      String raisedOnDate = row.get(2);
-      String description = row.get(3);
-      String assetNumber = row.get(4);
-
-      oneTicket.add(id);
-      oneTicket.add(ticketRaiser);
-      oneTicket.add(raisedOnDate);
-      oneTicket.add(description);
-      oneTicket.add(assetNumber);
-
-      expectedTickets.add(oneTicket);
+      expectedTicketsMap.put(id, row);
     }
 
     List<MaintenanceTicket> maintenanceTickets = assetPlus.getMaintenanceTickets();
-    assertNotNull(maintenanceTickets);
 
     for (var ticket : maintenanceTickets) {
-      assertNotNull(ticket);
-      List<String> oneTicket = new ArrayList<String>();
+      List<String> oneTicket = new ArrayList<>();
       String id = String.valueOf(ticket.getId());
       String ticketRaiser = ticket.getTicketRaiser().getEmail();
       String raisedOnDate = ticket.getRaisedOnDate().toString();
@@ -287,19 +272,11 @@ public class AddAndUpdateMaintenanceTicketStepDefinitions {
       oneTicket.add(description);
       oneTicket.add(assetNumber);
 
-      actualTickets.add(oneTicket);
+      actualTicketsMap.put(id, oneTicket);
     }
 
-    assertTrue(expectedTickets.size() == actualTickets.size());
-    if (expectedTickets.size() == actualTickets.size()) {
-      for (int i = 0; i < expectedTickets.size(); i++) {
-        for (int j = 0; j < expectedTickets.get(i).size(); j++) {
-          assertEquals(expectedTickets.get(i).get(j), actualTickets.get(i).get(j));
-
-        }
-      }
-    }
-
+    assertEquals(expectedTicketsMap.size(), actualTicketsMap.size());
+    assertEquals(expectedTicketsMap, actualTicketsMap);
   }
 
   /**
@@ -318,7 +295,6 @@ public class AddAndUpdateMaintenanceTicketStepDefinitions {
     Date date = Date.valueOf(dateString);
 
     MaintenanceTicket thisTicket = MaintenanceTicket.getWithId(id);
-    assertNotNull(thisTicket);
     assertEquals(ticketRaiser, thisTicket.getTicketRaiser());
     assertEquals(date, thisTicket.getRaisedOnDate());
     assertEquals(description, thisTicket.getDescription());
@@ -336,7 +312,8 @@ public class AddAndUpdateMaintenanceTicketStepDefinitions {
    */
   @Then("the ticket raised by {string} and with id {string}, date {string}, description {string}, and asset number {string} shall exist in the system \\(p16)")
   public void the_ticket_raised_by_and_with_id_date_description_and_asset_number_shall_exist_in_the_system_p16(
-      String email, String idString, String dateString, String description, String assetNumberString) {
+      String email, String idString, String dateString, String description,
+      String assetNumberString) {
 
     User ticketRaiser = User.getWithEmail(email);
     int id = Integer.valueOf(idString);
@@ -345,7 +322,6 @@ public class AddAndUpdateMaintenanceTicketStepDefinitions {
     SpecificAsset asset = SpecificAsset.getWithAssetNumber(assetNumber);
 
     MaintenanceTicket thisTicket = MaintenanceTicket.getWithId(id);
-    assertNotNull(thisTicket);
     assertEquals(ticketRaiser, thisTicket.getTicketRaiser());
     assertEquals(date, thisTicket.getRaisedOnDate());
     assertEquals(description, thisTicket.getDescription());
@@ -359,7 +335,6 @@ public class AddAndUpdateMaintenanceTicketStepDefinitions {
   @Then("the system shall raise the error {string} \\(p16)")
   public void the_system_shall_raise_the_error_p16(String string) {
     assertEquals(string, error);
-    assertEquals(1, errorCntr);
   }
 
   /**
@@ -369,7 +344,6 @@ public class AddAndUpdateMaintenanceTicketStepDefinitions {
   private void callController(String result) {
     if (!result.isEmpty()) {
       error += result;
-      errorCntr += 1;
     }
   }
 }
