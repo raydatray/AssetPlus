efc6f781e9a4b4bedcbd1db4946773fdb562826f
Ray <raydatray@gmail.com>
2023-11-27 22:35:30 -0500

    test run

--- src/main/java/ca/mcgill/ecse/assetplus/application/AssetPlusApplication.java
+++ src/main/java/ca/mcgill/ecse/assetplus/application/AssetPlusApplication.java
@@ -1,14 +1,16 @@
 package ca.mcgill.ecse.assetplus.application;
 
+import ca.mcgill.ecse.assetplus.javafx.fxml.AssetPlusFxmlView;
 import ca.mcgill.ecse.assetplus.model.AssetPlus;
 import ca.mcgill.ecse.assetplus.persistence.AssetPlusPersistence;
+import javafx.application.Application;
 
 public class AssetPlusApplication {
 
   private static AssetPlus assetPlus;
 
   public static void main(String[] args) {
-    // TODO Start the application user interface here
+    Application.launch(AssetPlusFxmlView.class, args);
   }
 
   public static AssetPlus getAssetPlus() {
--- src/main/java/ca/mcgill/ecse/assetplus/javafx/fxml/controllers/StdPageController.java
+++ src/main/java/ca/mcgill/ecse/assetplus/javafx/fxml/controllers/StdPageController.java
@@ -1,27 +1,75 @@
 package ca.mcgill.ecse.assetplus.javafx.fxml.controllers;
 
+
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+
+import ca.mcgill.ecse.assetplus.controller.TOMaintenanceTicket;
+import javafx.beans.property.SimpleStringProperty;
+
 import javafx.fxml.FXML;
+
 
 public class StdPageController {
   @FXML private Label pageTitle;
   @FXML private Button search;
   @FXML private Button toggleView;
   @FXML private Button addEntry;
+  @FXML private TableView dataTable;
 
-  String headers[] = null;
-  String items[] = null;
-
-  @FXML
-  private void initialize() {
-  
-  }
-  
-  public void addElements(){
+  public void viewTicketsButtonClicked() { 
     
-  }
+    TableColumn<TOMaintenanceTicket, String> ticketIdColumn = new TableColumn<>("Ticket ID");
+    ticketIdColumn.setCellValueFactory(cellData -> new SimpleStringProperty(Integer.toString(cellData.getValue().getId())));
+
+    TableColumn<TOMaintenanceTicket, String> raisedOnColumn = new TableColumn<>("Date Raised");
+    raisedOnColumn.setCellValueFactory(cellData -> new SimpleStringProperty(cellData.getValue().getRaisedOnDate().toString()));
+
+    TableColumn<TOMaintenanceTicket, String> descriptionColumn= new TableColumn<>("Description");  
+    descriptionColumn.setCellValueFactory(cellData -> new SimpleStringProperty(cellData.getValue().getDescription()));
+
+    TableColumn<TOMaintenanceTicket, String> ticketRaiserColumn = new TableColumn<>("Ticket Raiser");
+    ticketRaiserColumn.setCellValueFactory(cellData -> new SimpleStringProperty(cellData.getValue().getRaisedByEmail()));
+
+    TableColumn<TOMaintenanceTicket, String> assetNameColumn = new TableColumn<>("Asset Name");
+    ticketRaiserColumn.setCellValueFactory(cellData -> new SimpleStringProperty(cellData.getValue().getAssetName()));
+
+    TableColumn<TOMaintenanceTicket, String> expectedLifespanColumn = new TableColumn<>("Expected Lifespan");
+    ticketRaiserColumn.setCellValueFactory(cellData -> new SimpleStringProperty(Integer.toString(cellData.getValue().getExpectLifeSpanInDays())));
 
 
-    
+
+    dataTable.getColumns().add(ticketIdColumn);
+    dataTable.getColumns().add(ticketRaiserColumn);
+    dataTable.getColumns().add(raisedOnColumn);
+    dataTable.getColumns().add(descriptionColumn);
+    dataTable.getColumns().add(assetNameColumn);
+    dataTable.getColumns().add(expectedLifespanColumn);
+    TableColumn<TOMaintenanceTicket, String> purchaseDateColumn = new TableColumn<>("Purchase Date");
+    ticketRaiserColumn.setCellValueFactory(cellData -> new SimpleStringProperty(cellData.getValue().getPurchaseDate().toString()));
+    dataTable.getColumns().add(purchaseDateColumn);
+    TableColumn<TOMaintenanceTicket, String> floorNumberColumn = new TableColumn<>("Floor Number");
+    ticketRaiserColumn.setCellValueFactory(cellData -> new SimpleStringProperty(Integer.toString(cellData.getValue().getFloorNumber())));
+    dataTable.getColumns().add(floorNumberColumn);
+    TableColumn<TOMaintenanceTicket, String> roomNumberColumn = new TableColumn<>("Room Number");
+    ticketRaiserColumn.setCellValueFactory(cellData -> new SimpleStringProperty(Integer.toString(cellData.getValue().getRoomNumber())));
+    dataTable.getColumns().add(roomNumberColumn);
+    TableColumn<TOMaintenanceTicket, String> statusColumn = new TableColumn<>("Status");
+    ticketRaiserColumn.setCellValueFactory(cellData -> new SimpleStringProperty(cellData.getValue().getStatus()));
+    dataTable.getColumns().add(statusColumn);
+    TableColumn<TOMaintenanceTicket, String> fixedByColumn = new TableColumn<>("Fixed By Email");
+    ticketRaiserColumn.setCellValueFactory(cellData -> new SimpleStringProperty(cellData.getValue().getFixedByEmail()));
+    dataTable.getColumns().add(fixedByColumn);
+    TableColumn<TOMaintenanceTicket, String> timeToResolveColumn = new TableColumn<>("Time To Resolve");
+    ticketRaiserColumn.setCellValueFactory(cellData -> new SimpleStringProperty(cellData.getValue().getTimeToResolve()));
+    dataTable.getColumns().add(timeToResolveColumn);
+    TableColumn<TOMaintenanceTicket, String> priorityColumn = new TableColumn<>("Priority");
+    ticketRaiserColumn.setCellValueFactory(cellData -> new SimpleStringProperty(cellData.getValue().getPriority()));
+    dataTable.getColumns().add(priorityColumn);
+    TableColumn<TOMaintenanceTicket, String> requiresApprovalColumn = new TableColumn<>("Requires Manager Approval");
+    ticketRaiserColumn.setCellValueFactory(cellData -> new SimpleStringProperty(Boolean.toString(cellData.getValue().getApprovalRequired())));
+    dataTable.getColumns().add(requiresApprovalColumn);
+  } 
 }
