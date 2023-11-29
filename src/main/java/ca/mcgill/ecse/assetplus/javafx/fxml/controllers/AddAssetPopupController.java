11fa1c3782158af572d0463a31de01ac4a6080f0
Houman <veryhouman@gmail.com>
2023-11-28 22:02:45 -0500

    -Fix Controller for AddAsset

--- src/main/java/ca/mcgill/ecse/assetplus/javafx/fxml/controllers/AddAssetPopupController.java
+++ src/main/java/ca/mcgill/ecse/assetplus/javafx/fxml/controllers/AddAssetPopupController.java
@@ -43,6 +43,7 @@ public class AddAssetPopupController {
       popupStage.show();
     } catch (Exception e) {
       // Maybe prompt error pop up in case or error?
+      addButton.setStyle("-fx-background-color: red;");
       System.err.println("Error loading FXML: " + e.getMessage());
     }
   }
