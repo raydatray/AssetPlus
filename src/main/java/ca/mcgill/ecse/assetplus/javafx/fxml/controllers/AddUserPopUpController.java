44339fa90b346af875d500b327ba004aaa84d5a7
Alexander Liu <alexander_l_22@hotmail.com>
2023-11-29 00:38:42 -0500

    small change

--- src/main/java/ca/mcgill/ecse/assetplus/javafx/fxml/controllers/AddUserPopUpController.java
+++ src/main/java/ca/mcgill/ecse/assetplus/javafx/fxml/controllers/AddUserPopUpController.java
@@ -1,6 +1,5 @@
 package ca.mcgill.ecse.assetplus.javafx.fxml.controllers;
 
-import com.thoughtworks.xstream.mapper.SystemAttributeAliasingMapper;
 import ca.mcgill.ecse.assetplus.controller.AssetPlusFeatureSet1Controller;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
