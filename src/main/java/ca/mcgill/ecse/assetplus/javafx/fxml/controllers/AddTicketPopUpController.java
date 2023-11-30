3a8c9fd9690e8d962a94819104d3f03840c62304
Alexander Liu <alexander_l_22@hotmail.com>
2023-11-30 14:53:38 -0500

    Updated pop up for add ticket

--- src/main/java/ca/mcgill/ecse/assetplus/javafx/fxml/controllers/AddTicketPopUpController.java
+++ src/main/java/ca/mcgill/ecse/assetplus/javafx/fxml/controllers/AddTicketPopUpController.java
@@ -1,34 +1,70 @@
 package ca.mcgill.ecse.assetplus.javafx.fxml.controllers;
 
+import ca.mcgill.ecse.assetplus.controller.AssetPlusFeatureSet1Controller;
+import ca.mcgill.ecse.assetplus.controller.AssetPlusFeatureSet2Controller;
+import ca.mcgill.ecse.assetplus.controller.AssetPlusFeatureSet3Controller;
 import ca.mcgill.ecse.assetplus.controller.AssetPlusFeatureSet4Controller;
+import ca.mcgill.ecse.assetplus.controller.TOAssetType;
+import ca.mcgill.ecse.assetplus.controller.TOSpecificAsset;
+import ca.mcgill.ecse.assetplus.controller.TOUser;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.DatePicker;
 import javafx.scene.control.TextField;
 import javafx.scene.paint.Color;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
-import java.sql.Date;
 import java.text.ParseException;
-import java.text.SimpleDateFormat;
+import java.util.List;
 
 public class AddTicketPopUpController {
 
   @FXML private Button addTicketButton;
-  @FXML private TextField assetNumberTextField;
+  @FXML private ChoiceBox<String> assetNumberChoiceBox;
   @FXML private Button closePopUpButton;
-  @FXML private TextField dateRaisedTextField;
+  @FXML private DatePicker dateRaisedDatePicker;
   @FXML private TextField descriptionTextField;
-  @FXML private TextField raiserEmailTextField;
   @FXML private TextField ticketIdTextField;
 
   StdPageController topController;
 
   public AddTicketPopUpController(StdPageController headController){
     this.topController = headController;
+  }
+  @FXML private ChoiceBox<String> ticketRaiserEmailChoiceBox;
+
+  @FXML
+  public void populateChoiceBoxes() {
+    // Asset number choice box
+    List<TOSpecificAsset> assetList = AssetPlusFeatureSet3Controller.getSpecificAssets();
+
+    ObservableList<String> assetNumberList = FXCollections.observableArrayList();
+    
+    assetNumberList.addAll("-- Select an asset number --");
+    for (TOSpecificAsset asset : assetList) {
+      assetNumberList.add(String.valueOf(asset.getAssetNumber()));
+    }
+
+    assetNumberChoiceBox.setItems(assetNumberList);
+    assetNumberChoiceBox.setValue(assetNumberList.get(0));
+
+    // Ticket raiser email choice box
+    List<TOUser> userList = AssetPlusFeatureSet1Controller.getUsers();
+    ObservableList<String> userEmailList = FXCollections.observableArrayList();
+    userEmailList.addAll("-- Select a ticket raiser email -- ");
+    for (TOUser user : userList) {
+      userEmailList.add(user.getEmail());
+    }
+    ticketRaiserEmailChoiceBox.setItems(userEmailList);
+    ticketRaiserEmailChoiceBox.setValue(userEmailList.get(0));
+
   }
 
   @FXML
@@ -38,6 +74,10 @@ public class AddTicketPopUpController {
       FXMLLoader loader = new FXMLLoader(getClass().getResource("/ca/mcgill/ecse/assetplus/javafx/fxml/pop-ups/AddMaintenanceTicketPopUp.fxml"));
       loader.setControllerFactory(param -> new AddTicketPopUpController(topController));
       Parent root = loader.load();
+
+      AddTicketPopUpController controller = loader.getController();
+      controller.populateChoiceBoxes();
+
       // Create a new stage for the pop-up
       Stage popupStage = new Stage();
       popupStage.initModality(Modality.APPLICATION_MODAL);
@@ -57,16 +97,14 @@ public class AddTicketPopUpController {
 
   @FXML
   public void addMaintenanceTicket() throws NumberFormatException, ParseException {
-    String assetNumber = assetNumberTextField.getText();
-    String dateRaised = dateRaisedTextField.getText();
-    String description = descriptionTextField.getText();
-    String raiserEmail = raiserEmailTextField.getText();
-    String ticketId = ticketIdTextField.getText();
-
-    //SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
-
     try {
-      String error = AssetPlusFeatureSet4Controller.addMaintenanceTicket(Integer.parseInt(ticketId), Date.valueOf(dateRaised), description, raiserEmail, Integer.parseInt(assetNumber));
+      String assetNumber = assetNumberChoiceBox.getValue();
+      java.sql.Date dateRaised = java.sql.Date.valueOf(dateRaisedDatePicker.getValue());
+      String description = descriptionTextField.getText();
+      String raiserEmail = ticketRaiserEmailChoiceBox.getValue();
+      String ticketId = ticketIdTextField.getText();
+
+      String error = AssetPlusFeatureSet4Controller.addMaintenanceTicket(Integer.parseInt(ticketId), dateRaised, description, raiserEmail, Integer.parseInt(assetNumber));
       if (!error.equals("")) {
         ViewUtils.showError(error);
 
@@ -74,7 +112,7 @@ public class AddTicketPopUpController {
   
       }
       } catch (Exception e) {
-        ViewUtils.showError(e.getMessage());
+        ViewUtils.showError("Invalid inputs provided.");
       }
       finally {
         topController.refreshTable("Tickets");
